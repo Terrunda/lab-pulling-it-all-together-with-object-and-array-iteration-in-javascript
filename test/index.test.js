e879d273c91810cf5a202737d1409d7f0552b58c
Terrunda <terrencegift49@gmail.com>
2025-12-07 23:08:55 +0300

    Made sure that all Jest cases passed successfully.

--- test/index.test.js
+++ test/index.test.js
@@ -1,50 +1,44 @@
  const {numPointsScored, shoeSize, teamColors, teamNames, playerNumbers, playerStats, bigShoeRebounds, mostPointsScored, winningTeam, playerWithLongestName, doesLongNameStealATon} = require("../index.js");
 
-describe("Test case for numPointsScored",() => {
-  test("Normal case for numPointsScored", () => {
-    expect(numPointsScored("Alan Anderson").toBe(22));
-  });
-})
- 
-describe('Basketball Stats', function () {
+describe('Basketball Stats', () => {
   
   describe('numPointsScored', function () {
-    it('should return points scored by a player', function () {
-      expect(numPointsScored('Alan Anderson')).to.equal(22);
-      expect(numPointsScored('Ben Gordon')).to.equal(33);
+    test('should return points scored by a player', () => {
+      expect(numPointsScored('Alan Anderson')).toEqual(22);
+      expect(numPointsScored('Ben Gordon')).toEqual(33);
     });
   });
 
-  describe('shoeSize', function () {
-    it('should return shoe size of a player', function () {
-      expect(shoeSize('Alan Anderson')).to.equal(16);
-      expect(shoeSize('Ben Gordon')).to.equal(15);
+  describe('shoeSize',() => {
+    test('should return shoe size of a player', () => {
+      expect(shoeSize('Alan Anderson')).toEqual(16);
+      expect(shoeSize('Ben Gordon')).toEqual(15);
     });
   });
   
-  describe('teamColors', function () {
-    it('should return team colors', function () {
-      expect(teamColors('Brooklyn Nets')).to.deep.equal(['Black', 'White']);
-      expect(teamColors('Charlotte Hornets')).to.deep.equal(['Turquoise', 'Purple']);
+  describe('teamColors', () => {
+    test('should return team colors', () => {
+      expect(teamColors('Brooklyn Nets')).toEqual(['Black', 'White']);
+      expect(teamColors('Charlotte Hornets')).toEqual(['Turquoise', 'Purple']);
     });
   });
   
-  describe('teamNames', function () {
-    it('should return team names', function () {
-      expect(teamNames()).to.deep.equal(['Brooklyn Nets', 'Charlotte Hornets']);
+  describe('teamNames', () => {
+    test('should return team names', () => {
+      expect(teamNames()).toEqual(['Brooklyn Nets', 'Charlotte Hornets']);
     });
   });
 
-  describe('playerNumbers', function () {
-    it('should return player numbers of a team', function () {
-      expect(playerNumbers('Brooklyn Nets')).to.deep.equal([0, 30, 11, 1, 31]);
-      expect(playerNumbers('Charlotte Hornets')).to.deep.equal([4, 0, 2, 8, 33]);
+  describe('playerNumbers', () => {
+    test('should return player numbers of a team', () => {
+      expect(playerNumbers('Brooklyn Nets')).toEqual([0, 30, 11, 1, 31]);
+      expect(playerNumbers('Charlotte Hornets')).toEqual([4, 0, 2, 8, 33]);
     });
   });
 
   describe('playerStats', function () {
-    it('should return stats of a player', function () {
-      expect(playerStats('Alan Anderson')).to.deep.equal({
+    test('should return stats of a player',  () => {
+      expect(playerStats('Alan Anderson')).toEqual({
         number: 0,
         shoe: 16,
         points: 22,
@@ -55,7 +49,7 @@ describe('Basketball Stats', function () {
         slamDunks: 1
       });
 
-      expect(playerStats('Ben Gordon')).to.deep.equal({
+      expect(playerStats('Ben Gordon')).toEqual({
         number: 8,
         shoe: 15,
         points: 33,
@@ -68,9 +62,34 @@ describe('Basketball Stats', function () {
     });
   });
 
-  describe('bigShoeRebounds', function () {
-    it('should return number of rebounds for the player with the biggest shoe', function () {
-      expect(bigShoeRebounds()).to.equal(12);
+  describe('bigShoeRebounds', () => {
+    test('should return number of rebounds for the player with the biggest shoe',  () => {
+      expect(bigShoeRebounds()).toEqual(12);
     });
   });
+
+  describe("mostPointsScored", ()=> {
+    test("Should return a number indicating the most points scored from the gameData object", ()=>{
+      expect(mostPointsScored()).toEqual(33);
+    });
+  });
+
+  describe("winningTeam", ()=>{
+    test("Should return the winning team by comparing which team scored the most points", () => {
+      expect(winningTeam()).toEqual("Brooklyn Nets");
+    });
+  });
+
+  describe("playerWithLongestName", ()=>{
+    test("Should return the player with the longest name from the gameObject() data", ()=> {
+      expect(playerWithLongestName()).toEqual("Bismack Biyombo");
+    });
+  });
+
+  describe("doesLongNameStealaTon", () => {
+    test("Should return a boolean value indicating whether the player with the longest name has the most steals in a game obtained from gameObject", () => {
+      expect(doesLongNameStealATon()).toBe(false);
+    });
+  })
 });
+
